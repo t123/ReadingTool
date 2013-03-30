6d6feb98a9e4058a0c192b12c48f8b49e4a0c80d
Travis Watt <travis@readingtool.net>
2013-03-30 14:17:33 +0000

    Missing files

---
+++ ReadingTool.Common/Extensions/StringExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReadingTool.Common.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToBase64(this String theString)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(theString);
+            return Convert.ToBase64String(toEncodeAsBytes);
+        }
+    }
+}
---
+++ ReadingTool.Site/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ReadingTool.Site.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected Guid UserId
+        {
+            get { return Guid.Parse(HttpContext.User.Identity.Name); }
+        }
+    }
+}
