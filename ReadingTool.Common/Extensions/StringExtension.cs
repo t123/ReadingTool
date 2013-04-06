030caaac4659ea7ac931703a12a5745c9e27a5ac
Travis Watt <travis@readingtool.net>
2013-04-06 14:46:04 +0100

    Review term highlight fix

--- ReadingTool.Common/Extensions/StringExtension.cs
+++ ReadingTool.Common/Extensions/StringExtension.cs
@@ -16,5 +16,27 @@ namespace ReadingTool.Common.Extensions
 
             return theString.Replace("\n", "<br/>");
         }
+
+        public static string Replace(this string source, string oldString, string newString, StringComparison comp)
+        {
+            if(string.IsNullOrEmpty(source) || string.IsNullOrEmpty(oldString))
+            {
+                return source;
+            }
+
+            if(newString == null)
+            {
+                newString = "";
+            }
+
+            int index = source.IndexOf(oldString, comp);
+            if(index >= 0)
+            {
+                source = source.Remove(index, oldString.Length);
+                source = source.Insert(index, newString);
+            }
+
+            return source;
+        }
     }
 }
