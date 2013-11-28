3d2e66e9b9e60c2703767d169f3d5f7385392586
Travis Watt <travis.watt@gmail.com>
2013-11-28 14:15:51 +0000

    Changed row limit from 20 to 25

--- app/views/terms/index.blade.php
+++ app/views/terms/index.blade.php
@@ -19,7 +19,7 @@ Terms
         <select class="form-control input-sm" name="rowsPerPage" id="rowsPerPage">
             <option value="5">5</option>
             <option value="10">10</option>
-            <option value="20">20</option>
+            <option value="25">25</option>
             <option value="50">50</option>
             <option value="100">100</option>
         </select>
